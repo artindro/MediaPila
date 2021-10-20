c02fbf1a2473d05cc4012d2b162d3da9818d7086
milagroscarrascal <92893216+milagroscarrascal@users.noreply.github.com>
2021-10-20 19:04:50 -0300

    Add files via upload

---
+++ Login Design/js/main.js
@@ -0,0 +1,18 @@
+var contenedor_form = document.querySelector(".contenedor-form")
+var toggle_ = document.querySelector(".toggle")
+var formulario_ = document.querySelector(".formulario")
+
+
+// animación con js para Registarse
+$('.toggle').click(function () {
+    $('.formulario').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+});
+
+function() {
+    
+}
